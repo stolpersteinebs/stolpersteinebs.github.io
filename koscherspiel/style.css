fe18f6396bf7e7dbc4b4fb2c07bddee81324c3b5
3d-doktor <maxschnell@icloud.com>
2026-02-19 15:28:46 +0100

    Create style.css

---
+++ koscherspiel/style.css
@@ -0,0 +1,36 @@
+body {
+  margin: 0;
+  overflow: hidden;
+  background: #111;
+  font-family: Arial, sans-serif;
+}
+
+#game {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+}
+
+#player {
+  position: absolute;
+  bottom: 20px;
+  width: 80px;
+  height: 80px;
+  background: blue;
+  border-radius: 10px;
+}
+
+#score {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  color: white;
+  font-size: 24px;
+}
+
+.falling {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
